3411cd7ccd1c22eca616fcfb71f8eb0ab2c24f9a
tot-tech-lead <84918090+viktor-kindrat@users.noreply.github.com>
2025-07-20 20:07:38 +0300

    Google analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "@/lib/get-messages";
 import { locales, defaultLocale, isValidLocale } from "@/i18n";
+import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,49 +40,15 @@ export default async function LocaleLayout({
 
   return (
     <html lang={validLocale} className="scroll-smooth">
-      {/* Google Tag Manager */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-56GR76S2');`,
-        }}
-      />
-      {/* End Google Tag Manager */}
-
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-R1WNKEYCT2"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-R1WNKEYCT2');
-          `,
-        }}
-      />
-      {/* End Google tag (gtag.js) */}
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-black text-white min-h-screen`}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-56GR76S2"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider locale={validLocale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
+      <GoogleTagManager gtmId="GTM-56GR76S2" />
+      <GoogleAnalytics gaId="G-R1WNKEYCT2" />
     </html>
   );
 }
